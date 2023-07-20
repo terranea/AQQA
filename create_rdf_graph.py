6d8767cd4480007e45c8fbe7bdbe86ab658b1859
David Röbl <david.roebl@yahoo.com>
2023-07-20 13:15:22 +0200

    update sparql query

--- create_rdf_graph.py
+++ create_rdf_graph.py
@@ -1,14 +1,11 @@
-from rdflib import Graph, Literal, Namespace, RDF, URIRef, XSD
+import rdflib
+from rdflib import Graph, Literal, Namespace, RDF, URIRef, XSD, RDFS, SOSA
 import matplotlib.pyplot as plt
 import networkx as nx
 from rdflib.extras.external_graph_libs import rdflib_to_networkx_multidigraph
-from rdflib.plugins.sparql import prepareQuery
 
 # Define the namespaces
 aqqa = Namespace("http://example.com/ontologies/aqqa#")
-rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-sosa = Namespace("http://www.w3.org/ns/sosa/")
-rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
 geo = Namespace("http://www.opengis.net/ont/geosparql#")
 xsd = Namespace("http://www.w3.org/2001/XMLSchema#")
 
@@ -18,22 +15,22 @@ g.bind("aqqa", aqqa)
 
 
 # Ingest the RDF data
-g.add((aqqa.PM25, RDF.type, sosa.ObservableProperty))
-g.add((aqqa.PM25, rdfs.label, Literal('PM 2.5')))
+g.add((aqqa.PM25, RDF.type, SOSA.ObservableProperty))
+g.add((aqqa.PM25, RDFS.label, Literal('PM 2.5')))
 g.add((aqqa.PM25, aqqa.hasUnit, Literal('µg m-3')))
-g.add((aqqa.PM25, rdfs.comment, Literal('24 hour mean value of CAMS PM2.5 reanalysis data at surface')))
+g.add((aqqa.PM25, RDFS.comment, Literal('24 hour mean value of CAMS PM2.5 reanalysis data at surface')))
 
-g.add((aqqa.Cell_X, RDF.type, sosa.FeatureOfInterest))
+g.add((aqqa.Cell_X, RDF.type, SOSA.FeatureOfInterest))
 g.add((aqqa.Cell_X, aqqa.hasID, Literal(1, datatype=XSD.integer)))
 g.add((aqqa.Cell_X, geo.hasGeometry, aqqa.GeomCell_X))
 
 g.add((aqqa.GeomCell_X, RDF.type, geo.Geometry))
 g.add((aqqa.GeomCell_X, geo.asWKT, Literal("POINT (-73.935242 40.730610)", datatype=geo.wktLiteral)))
 
-g.add((aqqa.CellID_X_timestamp_1000_var_PM25, RDF.type, sosa.Observation))
-g.add((aqqa.CellID_X_timestamp_1000_var_PM25, sosa.observedProperty, aqqa.PM25))
-g.add((aqqa.CellID_X_timestamp_1000_var_PM25, sosa.hasSimpleResult, Literal(14.4)))
-g.add((aqqa.CellID_X_timestamp_1000_var_PM25, sosa.resultTime, Literal("2023-07-20T15:30:00", datatype=XSD.dateTime)))
+g.add((aqqa.CellID_X_timestamp_1000_var_PM25, RDF.type, SOSA.Observation))
+g.add((aqqa.CellID_X_timestamp_1000_var_PM25, SOSA.observedProperty, aqqa.PM25))
+g.add((aqqa.CellID_X_timestamp_1000_var_PM25, SOSA.hasSimpleResult, Literal(14.4)))
+g.add((aqqa.CellID_X_timestamp_1000_var_PM25, SOSA.resultTime, Literal("2023-07-20T15:30:00", datatype=XSD.dateTime)))
 
 
 # Print the graph to see the ingested data (optional)
@@ -50,24 +47,25 @@ nx.draw(g_nx, pos, with_labels=True, node_size=2000, node_color="skyblue", font_
 plt.show()
 """
 
-# Execute Query 1
-query1 = prepareQuery(
-    """SELECT ?observation ?label ?unit ?resultTime
+
+# Execute Query 1 (get variable, time and result of all observations)
+query1 = """    
+    SELECT ?var ?unit ?time ?measurement
        WHERE {
-         ?observableProperty rdf:type sosa:ObservableProperty ;
-                             rdfs:label "PM 2.5" ;
-                             aqqa:hasUnit ?unit .
-  
-         ?observation sosa:observedProperty ?observableProperty ;
-                      rdfs:label ?label ;
-                      sosa:resultTime ?resultTime .
+            ?s 
+               a sosa:Observation ;
+               sosa:resultTime ?time ;
+               sosa:hasSimpleResult ?measurement ;
+               sosa:observedProperty ?obsProp .
+            ?obsProp 
+                rdfs:label ?var ;
+                aqqa:hasUnit ?unit .
        }"""
-)
 
 results1 = g.query(query1)
 print("Query 1 Results:")
 for row in results1:
-    print(f"Observation: {row['observation']}, Label: {row['label']}, Unit: {row['unit']}, Result Time: {row['resultTime']}")
+    print(f"{row['time']} {row['measurement']} {row['unit']} {row['var']}")
 
 
 
