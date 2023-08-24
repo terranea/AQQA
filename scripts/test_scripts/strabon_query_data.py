a84846bded96070c803543797688c6596c0ae38c
14roebl <david.roebl@yahoo.com>
2023-08-24 07:52:50 +0000

    implement geosparql query

--- scripts/test_scripts/strabon_query_data.py
+++ scripts/test_scripts/strabon_query_data.py
@@ -8,9 +8,7 @@ if __name__ == "__main__":
     # Create a SPARQLWrapper instance with the endpoint URL
     sparql = SPARQLWrapper(endpoint_url)
 
-    # Set the SPARQL query
-    sparql.setQuery(
-    """   
+    query =     """   
     PREFIX aqqa: <http://example.com/ontologies/aqqa#>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX sosa: <http://www.w3.org/ns/sosa/>
@@ -30,7 +28,28 @@ if __name__ == "__main__":
                 rdfs:label ?var ;
                 aqqa:hasUnit ?unit .
     }"""
-    )
+
+    query = """
+    PREFIX gadm: <http://example.com/ontologies/gadm#> 
+    PREFIX geo: <http://www.opengis.net/ont/geosparql#> 
+    PREFIX geof: <http://www.opengis.net/def/function/geosparql/>
+
+    SELECT ?name ?lvl
+    WHERE {
+        ?s a gadm:AdministrativeUnit ;
+            gadm:hasName ?name ;
+            gadm:hasNationalLevel ?lvl ;
+            geo:hasGeometry ?geom_ent .
+
+        ?geom_ent geo:asWKT ?geom .
+        
+        FILTER (geof:sfContains(?geom, 'POINT(9.729870 48.330776)'^^geo:wktLiteral))
+    } 
+    LIMIT 10
+    """
+
+    # Set the SPARQL query
+    sparql.setQuery(query)
 
     # Set the return format to JSON
     sparql.setReturnFormat(JSON)
@@ -39,6 +58,6 @@ if __name__ == "__main__":
        # Execute the query and get the results
        ret = sparql.queryAndConvert()
        for r in ret["results"]["bindings"]:
-          print(f"{r['time']['value']} {r['measurement']['value']} {r['unit']['value']} {r['var']['value']}")
+          print(f"{r['name']['value']} {r['lvl']['value']}")
     except Exception as e:
         print(e)
