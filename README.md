bfbaf6c93ef63a66a5916c364f1c1bbdd61eba59
14roebl <david.roebl@yahoo.com>
2023-08-31 17:26:35 +0000

    preprocess cams aq data script now working; README for this function updated

--- README.md
+++ README.md
@@ -17,16 +17,40 @@ Before using this script, make sure you have the following:
 python download_cams_aq_data.py --year YEAR --month MONTH --variables VARIABLE1 VARIABLE2 ... --type DATA_TYPE --output-path OUTPUT_PATH
 ```
 
-Replace the placeholders:
-
+#### Options
 YEAR: Year for which you want to download the data (e.g., 2020).
 MONTH: Month for which you want to download the data (e.g., 01).
 VARIABLE1 VARIABLE2 ...: List of variables to download (e.g., carbon_monoxide nitrogen_dioxide ozone).
 TYPE: Type of data to download (e.g., validated_reanalysis or interim_reanalysis). At current date validated_reanalysis is only availabe up to 2020
 OUTPUT_PATH: Path to save the downloaded data (e.g., /mnt/data/raw/cams_euro_aq_reanalysis/2020/download_2020_01.zip).
 
-Example:
+#### Exampole:
 ```bash
 python download_cams_aq_data.py --year 2020 --month 01 --variables carbon_monoxide nitrogen_dioxide ozone --type validated_reanalysis --output-path /path/to/output.zip
 ```
 
+
+## CAMS Air Quality Data Preprocessing
+
+This Python script allows you to preprocess CAMS (Copernicus Atmosphere Monitoring Service) air quality data. The script extracts specific variables from zipped NetCDF files, clips them to a specified bounding box, and performs temporal aggregation before saving the results as new NetCDF files.
+
+### Usage 
+
+```bash
+python preprocess_cams_aq_data.py --path-to-input path_to_zip_folder --bounding-box lat_min lat_max lon_min lon_max --path-to-output path_to_output_folder
+```
+
+Replace the placeholders path_to_zip_folder, lat_min, lat_max, lon_min, lon_max, and path_to_output_folder with your actual values.
+
+#### Options
+--path-to-input: Required. Path to the zipped folder containing CAMS AQ data.
+--bounding-box: Required. Specify the bounding box for your area of interest. Provide four float values: lat_min, lat_max, lon_min, lon_max.
+--path-to-output: Required. Path to the location where unzipped and preprocessed NetCDF files will be stored.
+
+#### Example
+```bash
+python preprocess_cams_aq_data.py --path-to-input data.zip --bounding-box 45.82 50.65 8.95 17.22 --path-to-output processed_data
+```
+
+#### Output
+Processed NetCDF files with aggregated and clipped data will be saved in the specified --path-to-output folder.
--- scripts/production_scripts/preprocess_cams_aq_data.py
+++ scripts/production_scripts/preprocess_cams_aq_data.py
@@ -1,60 +1,57 @@
-
 import os
 import xarray as xr
 import zipfile
 import glob
 import argparse
 from utils import clip_netcdf_to_bb
+from zipfile import ZipFile
 
-
-def preprocess_cams_aq_data(path_to_zip_folder: str, bb: list, path_to_output: str):
-
-    # unzip cams aq data
-    print(path_to_zip_folder)
-    path_to_zip_folder = "/mnt/data/raw/cams_euro_aq_reanalysis/2020/download_2020_01.zip"
+def preprocess_cams_aq_data(path_to_zip_folder: str, bounding_box: list, path_to_output: str):
+    # Unzip CAMS AQ data
     with zipfile.ZipFile(path_to_zip_folder, 'r') as zip_ref:
-        zip_ref.extractall(path_to_unzip_folder)
+        zip_ref.extractall(path_to_output)
     
-    # get directories to individual nc files
-    search_string = f"{path_to_unzip_folder}/*"
+    # Get paths to individual nc files
+    search_string = os.path.join(path_to_output, "*.nc")
     path_to_files = glob.glob(search_string)
 
-    ds_dict = {}
-
     for path_to_file in path_to_files:
-
-        # extract variable from path name
+        # Extract variable from path name
         var = path_to_file.split(".")[-4]
 
-        # open dataset
+        # Open dataset
         ds = xr.open_dataset(path_to_file)
 
-        # clip data to bounding box
+        # Clip data to bounding box
         ds_bb = clip_netcdf_to_bb(ds, bounding_box)
 
-        # period averaging
+        # Period averaging
         if var == "O3":
             ds_bb_agg = ds_bb.resample(time="8H").mean()
         else:
-            ds_bb_agg= ds_bb.resample(time="24H").mean()
+            ds_bb_agg = ds_bb.resample(time="24H").mean()
 
-        # save netcdf 
-        path_output_variable = os.path.join(path_to_output, f"cams_aq_{var}_{year}{month}.nc")
+        # Save netcdf
+        year_month = ds_bb_agg.time.dt.strftime("%Y%m").values[0]
+        path_output_variable = os.path.join(path_to_output, f"cams_aq_{var}_{year_month}.nc")
         ds_bb_agg.to_netcdf(path_output_variable)
 
+        # delete extracted file
+        os.remove(path_to_file)
+
+        print(f"{var}-file processed!")
 
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser(description="Preprocess CAMS air quality data (clip to bb and temporally aggregate).")
     
-    # specify CLI arguments
-    parser.add_argument("--path-to-input", required=True, help="Path to zipped folder where nc files with CAMS AQ data is stored")
-    parser.add_argument("--bounding-box", required=True,  nargs="+", help="Specify bounding box for your area of interest e.g. [45.82, 50.65, 8.95, 17.22]")
-    parser.add_argument("--path-to-output", required=True, help="Path to location where unzipped preprocessed nc files should be stored")
+    # Specify CLI arguments
+    parser.add_argument("--path-to-input", required=True, help="Path to zipped folder containing CAMS AQ data.")
+    parser.add_argument("--bounding-box", required=True, nargs="+", type=float, help="Specify bounding box for your area of interest e.g. 45.82 50.65 8.95 17.22")
+    parser.add_argument("--path-to-output", required=True, help="Path to location where unzipped preprocessed nc files should be stored.")
     
     args = parser.parse_args()
-    
     preprocess_cams_aq_data(args.path_to_input, args.bounding_box, args.path_to_output)
+
 
 
 
