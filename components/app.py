f58351ec18eea66f567548eaf3812c9b3de2f879
14roebl <david.roebl@yahoo.com>
2023-10-16 16:24:25 +0000

    streamlit almost working except geojson load

--- components/app.py
+++ components/app.py
@@ -1,34 +1,74 @@
 import streamlit as st
+import pandas as pd
 import folium
 from streamlit_folium import st_folium
 from aq_kg_text_interface.openai_text_to_sparql import text_to_sparql
+from aq_kg_query.strabon_query_data import query_sparql_endpoint, convert_query_output_to_geojson, json_to_dataframe
 
-# Set the width for the sidebar
-st.markdown(
-    """
-    <style>
-    .sidebar .sidebar-content {
-        width: 300px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+# set screen wide
+st.set_page_config(layout="wide")
 
-# Create a Streamlit sidebar
+# initialize state variables
+if 'sparql_query' not in st.session_state: 
+    st.session_state['sparql_query'] = ""
+if 'query_results' not in st.session_state:
+    st.session_state['query_results'] = ""
+if 'query_results_geojson' not in st.session_state:
+    st.session_state['query_results_geojson'] = ""
+
+# create map object
+map_ = folium.Map(location=[51.5074, -0.1278], zoom_start=7)
+
+# Create a sidebar elements
 st.sidebar.title("Sidebar")
 text_input = st.sidebar.text_input("Enter text here")
+convert_button = st.sidebar.button("Convert")
+st.session_state['sparql_query'] = st.sidebar.text_area("Sparql query", value=st.session_state['sparql_query'])
+query_button = st.sidebar.button("Query")
 
-# Button to commit the action
-if st.sidebar.button("Commit"):
-    sparql_query = text_to_sparql(text_input)
-    st.sidebar.text(sparql_query)
+# Split main page into two columes
+col1, col2 = st.columns([2, 1])
 
-# Create a Folium map
-st.title("Interactive Map")
-map_ = folium.Map(location=[0, 0], zoom_start=2)
+# Map Column
+with col1:
+    st.header("Map")
+    folium.LayerControl().add_to(map_)
+    out = st_folium(map_, height=650, width=650)
 
-# Render the Folium map in Streamlit
-st.markdown('### Map')
-folium.LayerControl().add_to(map_)
-out = st_folium(map_, height=650, width=650)
+# Result Column
+with col2:
+    st.header("Results")
+    if isinstance(st.session_state["query_results"], pd.DataFrame):
+        st.dataframe(st.session_state["query_results"])
+    else:
+        st.dataframe()
+
+# Button to convert text to sparql query
+if convert_button:
+    if not text_input:
+        st.sidebar.error("There is no text")
+    else:
+        # convert text to sparql query
+        sparql_query = text_to_sparql(text_input)
+        st.session_state['sparql_query'] = sparql_query
+        st.experimental_rerun()
+
+# Butto to query the databse
+if query_button:
+    if st.session_state['sparql_query'] == "":
+        st.sidebar.error("There is no query")
+    else:
+        # retrieve results from sparql endpoint
+        results = query_sparql_endpoint(st.session_state['sparql_query'])
+        df_results = json_to_dataframe(results)
+        geojson_results = convert_query_output_to_geojson(results)
+        st.session_state["query_results"] = df_results
+        st.session_state["query_results_geojson"] = geojson_results
+
+        folium.GeoJson(st.session_state["query_results_geojson"], name="query_results").add_to(map_)
+        folium.Marker(location=[51.5074, -0.1278], popup="London").add_to(map_)
+
+        st.experimental_rerun()
+
+
+
--- components/aq_kg_query/strabon_query_data.py
+++ components/aq_kg_query/strabon_query_data.py
@@ -2,16 +2,77 @@ import geojson
 import json
 import shapely.wkt
 from SPARQLWrapper import SPARQLWrapper, JSON
-from config import STRABON_SPARQL_ENDPOINT
+from .config import STRABON_SPARQL_ENDPOINT
+import pandas as pd
+
+
+def query_sparql_endpoint(sparql_query: str):
+    """query sparql endpoint"""
+
+    sparql = SPARQLWrapper(STRABON_SPARQL_ENDPOINT)
+    sparql.setQuery(sparql_query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.queryAndConvert()
+
+    return results
+
+
+def convert_query_output_to_geojson(results: dict):
+    """convert query results about cams observations to geojson"""
+
+    try:
+        # Execute the query and get the results
+        features = []
+        for r in results["results"]["bindings"]:
+            geom_wkt = r["foi_geom"]["value"]
+            obs_time = r["obs_time"]["value"]
+            obs_result = r["obs_result"]["value"]
+
+            geom = shapely.wkt.loads(geom_wkt)
+            feature = geojson.Feature(geometry=geom, properties={"observation_time": obs_time, "observation_result": obs_result})
+            features.append(feature)
+
+        feature_collection = {"type": "FeatureCollection", "features": features}
+        
+        return feature_collection
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def json_to_dataframe(data):
+    """This function convert the results of a sparql query to a dataframe"""
+
+    # Check if the "bindings" key exists in the JSON
+    if "results" in data and "bindings" in data["results"]:
+        # Extract the "bindings" part from the JSON
+        bindings = data["results"]["bindings"]
+        # Create an empty list to store the data in a structured format
+        formatted_data = []
+
+        for entry in bindings:
+            # Create a dictionary to store the data for each entry
+            entry_data = {}
+            
+            # Iterate through the entry's fields
+            for field_name, field_data in entry.items():
+                entry_data[field_name] = field_data.get("value")
+
+            formatted_data.append(entry_data)
+
+        # Create a pandas DataFrame from the formatted data
+        df = pd.DataFrame(formatted_data)
+        df.drop_duplicates(inplace=True)
+        return df
+    else:
+        print("Invalid JSON format. Unable to convert to DataFrame.")
+        return None
 
 
 if __name__ == "__main__":
 
-    # Create a SPARQLWrapper instance with the endpoint URL
-    sparql = SPARQLWrapper(STRABON_SPARQL_ENDPOINT)
-
-    # Provide a list of CO concentration measurements in Geltendorf during January 2020.
-    sparql_query = """
+    query = """
     PREFIX sosa: <http://www.w3.org/ns/sosa/>
     PREFIX geo: <http://www.opengis.net/ont/geosparql#>
     PREFIX geof: <http://www.opengis.net/def/function/geosparql/>
@@ -26,7 +87,7 @@ if __name__ == "__main__":
                 ?foi_ent a sosa:FeatureOfInterest ;
                     geo:intersects ?gadm_ent .
                 ?gadm_ent a gadm:AdministrativeUnit ;
-                    gadm:hasName 'Oberösterreich' ;
+                    gadm:hasName 'Geltendorf' ;
             } 
         }
 
@@ -46,29 +107,15 @@ if __name__ == "__main__":
     }
     """
 
-    # Set the SPARQL query
-    sparql.setQuery(sparql_query)
-    sparql.setReturnFormat(JSON)
-    results = sparql.queryAndConvert()
+    # query strabon endpoint
+    results = query_sparql_endpoint(query)
+
+    # create geojson if possible
+    cams_data_geojson = convert_query_output_to_geojson(results)
+
+    # print geojson
+    print(cams_data_geojson)
 
 
-    try:
-        # Execute the query and get the results
-        features = []
-        for r in results["results"]["bindings"]:
-            geom_wkt = r["foi_geom"]["value"]
-            obs_time = r["obs_time"]["value"]
-            obs_result = r["obs_result"]["value"]
 
-            geom = shapely.wkt.loads(geom_wkt)
-            feature = geojson.Feature(geometry=geom, properties={"observation_time": obs_time, "observation_result": obs_result})
-            features.append(feature)
-
-        feature_collection = {"type": "FeatureCollection", "features": features}
-
-        with open("/mnt/data/output.geojson", "w") as f:
-            json.dump(feature_collection, f, indent=2)
-
-    except Exception as e:
-        print(e)
     
